b08635ac50a30596ac4a52cb44a5c8ccaee948eb
giga.freijo <giga.freijo@f7028cde-2b56-0410-aa73-abc337242cd6>
2008-09-14 22:14:17 +0000

    Ejercicios del 1 al 10 terminados
    TODO:
    JAY:
    11-14

--- trunk/tp1/src/generico.hs
+++ trunk/tp1/src/generico.hs
@@ -1,3 +1,4 @@
+module Generico where
 
 type Sentence s = s -> s
 type Combinator s = Sentence s -> Sentence s -> Sentence s
@@ -11,37 +12,27 @@ s >-> y = y.s 					-- s es algo q toma un parametro. Lo mismo para y
 
 -- Ejercicio 2
 (<-<) :: Combinator s 	
-s <-< y = s.y 					-- s es algo q toma un parametro. Lo mismo para y
+(<-<) = flip (>->) 					-- s es algo q toma un parametro. Lo mismo para y
 
 
 -- Ejercicio 3
 secAbs :: Combinator s -> [Sentence s] -> Sentence s
-secAbs c [] 	= identity
-secAbs c (x:xs) = c x (secAbs c xs)
+secAbs c xs = foldr c identity xs
 
 
 -- Ejercicio 4
 sec :: [Sentence s] -> Sentence s
-sec [] 		= identity
-sec (x:xs)	= (x >-> sec xs)
+sec xs = secAbs (>->) xs
+
 
 -- Ejercicio 5
 (><) :: Combinator s -> Sentence s -> Combinator s
-c >< s = auxiliar c s
-
-auxiliar :: Combinator s -> Sentence s -> Sentence s -> Sentence s -> Sentence s
-auxiliar c s1 s2 s3 = c s2 s3.c s1 s1			-- s1 es la que hay que anteponer
+c >< s = (\ s1 s2 -> (s >-> s) >-> c s1 s2)
 
 
 -- Ejercicio 6
-bifurcacion :: Predicate r -> Combinator s
-bifurcacion p = bifAux p
-
-bifAux :: Predicate r -> Sentence s -> Sentence s -> Sentence s			-- NO ANDA!!
-bifAux p s1 s2 = if (p) then
-					s1
-				 else
-				 	s2
+bifurcacion :: Predicate r -> Combinator r
+bifurcacion p = (\ s1 s2 str -> if (p str) then s1 str else s2 str)
 
 --especific sentences	
 identity :: Sentence s 
@@ -68,8 +59,7 @@ falseSentence r = False
 	- (restaUno >-> doble) 3 = 4
 	- (restaUno <-< doble) 3 = 5
 	- ( secAbs (>->) [doble, restaUno] ) 2
+	- (sec [doble, restaUno]) 2
 	- ((<-<) >< doble) restaUno doble 5	
 	
 -}
-
-
