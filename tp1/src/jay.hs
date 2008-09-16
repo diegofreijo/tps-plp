ebcbd06c631c3ee14a93c46a8f2514c241cdb6ae
gonzalo.A.R <gonzalo.A.R@f7028cde-2b56-0410-aa73-abc337242cd6>
2008-09-16 01:02:54 +0000

    El 11 terminado pero de una forma que no es valida segun el TP. Como esta ahora, en caso que la variable este definida, no le cambia el valor sino que se queda con el viejo. Esto quedo asi porque no pude hacerlo de la otra forma y no queria subir una version que no compilaba. En el archivo esta detallado el problema q no pude resolver y porq no anda como tiene que andar

--- tp1/src/jay.hs
+++ tp1/src/jay.hs
@@ -11,14 +11,57 @@ con c s = c
 var :: String -> JayExpression Int
 var varName s = maybe 0 id (lookup varName s)
 
-
 -- Ejercicio 11
-{-asig :: String -> JayExpression Int -> JaySentence
-asig nombre valor estado = [(nombre, valor estado)] ++ estado
+{--
+	- No se puede usar bifurcacion porque bifurcacion toma dos Sentences, cada una de ellas toma el mismo parametro que toma bifurcacion y lo 
+	devuelve transformado. El problema es que a bifurcacion necesitamos pasarle una 3-upla (key, value, estado), pero no podemos hacer una Sentece
+	que devuelva eso porque segun el tipo de asig esta tiene que devolver un estado => no se puede usar bifurcacion porq no dan los tipos
+--}
+asig :: String -> JayExpression Int -> JaySentence
+--asig nombre valor estado = [(nombre, valor estado)] ++ estado 
+--asig nombre valor estado = bifurcacion varPredicate (\t -> [(fst t, fst.snd t snd.snd t)] ++ snd.snd t) (nombre,(valor,estado)) (\t -> concatMap () )
+asig nombre valor estado = 	if (var nombre estado == 0) then
+								[(nombre, valor estado)] ++ estado 
+							else
+								{-- FIXME
+									- La linea correcta para el else es "1", pero no anda ( la "2" es un ejemplo simplificado )
+									  Lo que quiero hacer es recorrer la lista, para el elemento que coincida, le cambio la segunda
+									  coordenada a la tupla por el value que le paso, que representa el nuevo que se le esta asignando.
+									  No entiendo por q no anda la puta madre....
+									NOTE : como esta ahora, el comportamiento es:
+										- si no esta la variable en el estado, lo agrego
+										- si esta, no le cambio el valor, se queda con el que estaba
+								--1 concatMap (\key value elem -> if (key == fst elem) then (key, value) else elem) estado
+								--2 concatMap (\x -> x) estado
+								--}
+								estado		-- puse esto para que compile
 
+
+
+{--
+pruebaBif :: String -> JaySentence -> JayExpression Int 
+pruebaBif nombre valor estado = ( bifurcacion (\tup -> var (fst tup) (snd tup) == 0) (nombre,estado) ) 5 6 
+--}
+
+
+{--
+varPredicate :: Predicate (String,(Int,JayState))
+varPredicate tup = var (fst tup) (snd.snd tup) == 0
+--}
+
+{--
+isIn :: Predicate (String, JaySentence)
+isIn tup = (\tup -> var (fst tup) (snd tup) == 0) 
+--}
+
+test :: String -> JayExpression Int 
+test s j = (\tup -> var (fst tup) (snd tup) ) (s,j)
+
+
+{--
 pruebaBif :: String -> JayExpression Int -> JaySentence -> Int
 pruebaBif nombre valor estado = bifurcacion ((\tup -> (var (fst tup) (snd tup) == 0)) (nombre,estado)) 5 6-}
-
+--}
 
 -- Ejercicio 12
 op :: (a -> b -> c) -> JayExpression a -> JayExpression b -> JayExpression c
