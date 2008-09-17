277935a5145f176b679f6f938fe1997a03608a8f
matiasblanco18 <matiasblanco18@f7028cde-2b56-0410-aa73-abc337242cd6>
2008-09-17 05:29:01 +0000

    Comentarios agregados a generico.hs.
    blue

--- trunk/tp1/src/generico.hs
+++ trunk/tp1/src/generico.hs
@@ -4,62 +4,69 @@ type Sentence s = s -> s
 type Combinator s = Sentence s -> Sentence s -> Sentence s
 type Predicate s = s -> Bool
 
--- generics operators
 -- Ejercicio 1
 (>->) :: Combinator s 	
-s >-> y = y.s 					-- s es algo q toma un parametro. Lo mismo para y
-
+s >-> y = y.s 					-- Como en esta funcion, al parametro primero hay q aplicarle s y despues y, es utiliza el operador "."
+								-- que primero aplica al parametro la funcion de la derecha y luego al resultado de esto, la de la izquierda.
 
 -- Ejercicio 2
 (<-<) :: Combinator s 	
-(<-<) = flip (>->) 					-- s es algo q toma un parametro. Lo mismo para y
-
+(<-<) = flip (>->) 					-- Similar al anterior, pero esta vez se ejecuta primero el segundo combinador y luego el primero. Se utilizo
+									-- flip aplicado al ejercicio 1 para reutilizar codigo, ya que flip invierte los parametros dados, haciendo que
+									-- "a -> b -> c" se transforme en "b -> a -> c"
 
 -- Ejercicio 3
 secAbs :: Combinator s -> [Sentence s] -> Sentence s
-secAbs c xs = foldr c identity xs
-
+secAbs c xs = foldr c identity xs	-- Esta funcion lo que hace es aplicar a un parametro dado, una sucesion de sentencias de acuerdo con el combinador 
+									-- dado. Al utilizas foldr, se va aplicando el combinador a la lista de sentencias recursivamente, para finalmente 
+									-- aplicarlo sobre el parametro.
 
 -- Ejercicio 4
-sec :: [Sentence s] -> Sentence s
-sec xs = secAbs (>->) xs
+sec :: [Sentence s] -> Sentence s	
+sec = secAbs (>->) 					-- Similar al anterior, solo que en este caso, el combinador esta "hardcodeado" en >-> para ejecucion secuncial.
 
 
 -- Ejercicio 5
-(><) :: Combinator s -> Sentence s -> Combinator s
+(><) :: Combinator s -> Sentence s -> Combinator s 
 c >< s = (\ s1 s2 -> (s >-> s) >-> c s1 s2)
+									-- En este caso, el combinador dado ejecuta dos veces la sentencia s antes de aplicar las dos sentencias s1 y s2
+									-- en secuencia.
 
 
 -- Ejercicio 6
 bifurcacion :: Predicate r -> Combinator r
-bifurcacion p = (\ s1 s2 str -> if (p str) then s1 str else s2 str)
+bifurcacion p = (\ s1 s2 a -> if (p a) then s1 a else s2 a)
+									-- Se utilizo una funcion lambda para claridad del codigo. Dependiendo de la variable booleana resultante del
+									-- predicado a, se realiza una sentencia especifica. Si a es "True", entonces se ejecuta la primer sentencia
+									-- pasada como parametro. En cambio, si es "False", se ejecuta la segunda.
 
---especific sentences	
+									
+-- Sentencias utilizadas para probar los operadores genericos
 identity :: Sentence s 
-identity x = x
+identity x = x				-- Identidad
 
 doble :: Sentence Int
-doble x = x * 2
+doble x = x * 2				-- Multiplica por dos el parametro
 
-triple :: Sentence Int
-triple x = x * 3
+triple :: Sentence Int	
+triple x = x * 3			-- Multiplica por tres el parametro
 
-restaUno :: Sentence Int
-restaUno x = x - 1
+restaUno :: Sentence Int	
+restaUno x = x - 1			-- Resta uno al parametro
 
-trueSentence :: Predicate s
-trueSentence r = True 
+trueSentence :: Predicate s 
+trueSentence r = True 		-- Predicado True
 
 falseSentence :: Predicate s
-falseSentence r = False
+falseSentence r = False		-- Predicado False
 
 
 {- Ejemplos :
 	
 	- (restaUno >-> doble) 3 = 4
 	- (restaUno <-< doble) 3 = 5
-	- ( secAbs (>->) [doble, restaUno] ) 2
-	- (sec [doble, restaUno]) 2
-	- ((<-<) >< doble) restaUno doble 5	
+	- ( secAbs (<-<) [doble, restaUno] ) 2 = 2
+	- (sec [doble, restaUno]) 2 = 3
+	- ((<-<) >< doble) restaUno doble 5	 = 39
 	
 -}
