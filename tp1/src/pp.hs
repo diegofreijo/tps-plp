7a4ce6811ac88660a45924bfdbf62c6fd2a2b411
gonzalo.A.R <gonzalo.A.R@f7028cde-2b56-0410-aa73-abc337242cd6>
2008-09-18 00:57:01 +0000

    Agregue comentarios sobre lo que hace cada funcion (comentarios triviales, pero buen..)

--- tp1/src/pp.hs
+++ tp1/src/pp.hs
@@ -6,22 +6,22 @@ type PPSentence = Sentence PPState
 
 -- Ejercicio 7
 shl :: PPSentence
-shl s = tail (s ++ [(head s)])
+shl s = tail (s ++ [(head s)])						-- shl s equivale a apendear el head(s) y quedarse con el tail de la secuencia
 
 -- Ejercicio 8
 shr :: PPSentence
-shr s = sec (take (length s - 1) (repeat shl)) s
+shr s = sec (take (length s - 1) (repeat shl)) s	-- con la idea de reutilizar codigo, pensamos shr s = (shl s) repeteidas len s - 1 veces
 
 -- Ejercicio 9
 dup :: PPSentence
-dup = foldr (\x xs -> [x] ++ [x] ++ xs) []
+dup = foldr (\x xs -> [x] ++ [x] ++ xs) []			-- en cada iteracion, duplicamos el elemento actual (head de la lista) y se apendea a la lista resultante
 
 -- Ejercicio 10
 forN :: Int -> [PPSentence] -> PPSentence
-forN n xs = sec (flatten (take n (repeat xs)))
+forN n xs = sec (flatten (take n (repeat xs)))		-- primero aplanamos la lista de listas y luego reutilizamos la funcion 'sec'
 
 flatten :: [[a]] -> [a]
-flatten = concatMap (\x -> x)
+flatten = concatMap (\x -> x)						-- aplana una lista de listas a una lista
 
 
 
