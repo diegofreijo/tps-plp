50689d8a6b57c45cba4374d616f941ea68f6775b
matiasblanco18 <matiasblanco18@f7028cde-2b56-0410-aa73-abc337242cd6>
2008-10-27 00:08:08 +0000

    ej3 y ej4

--- tp2/src/tp.pl
+++ tp2/src/tp.pl
@@ -2,8 +2,6 @@ ruta(lujan, baires, 60).
 ruta(baires, laPlata, 70).
 ruta(laPlata, lujan, 140).
 
-
-
 ej1(M, Cs) :- withoutRepeated(M), withoutRepeated(CS), routesIncluded(M, Cs), citiesIncluded(Cs, M).
 
 
@@ -24,8 +22,21 @@ ej2(M, C, [X|Xs]) :- setOf(P, esVecino(M, C, X), L), M =:= L, ej2(M, C, Xs).
 esVecino(M,X,Y) :- member(ruta(Y,X,Z), M).
 esVecino(M,X,Y) :- member(ruta(X,Y,Z), M).
 
- 
+/* ej3 */
 
+ej3([ruta(C1,C2,Z)|MS],C1,C2,N) :- (Z = N).
+ej3([ruta(C2,C1,Z)|MS],C1,C2,N) :- (Z = N).
+ej3([ruta(_,_,Z)|MS],C1,C2,N)   :- (1 = 0);ej3(MS,C1,C2,N).
+
+/* ej4 */
+/* Agregar without repeated cuando este andando*/
+ej4(M,O,D,[])      :- (O=D).
+ej4(M,O,D,[C|Cs])  :- hayCamino(M,O,C),ej4(M,C,D,Cs).
+
+hayCamino(M,O,D) :- esVecino(M,O,D);(O=D).
+
+/* ej5 */
+ej5([ruta(O,D,N)|Ms]) = noReflexivo(
 
 /*
 isPresent(X, CS) :- member(X, CS).
@@ -34,7 +45,7 @@ isPresent(X, CS) :- member(X, CS).
 
 
 
-/*				ejemplo boludo
+/*                              ejemplo boludo
 append([], L, L).
 append([X|XS], L, [X|R]) :- append(XS, L, R).
 
@@ -44,5 +55,3 @@ equals([X|XS], [Y|YS]) :- X = Y, equals(XS, YS).
 sameSize([], []).
 sameSize([M|MS], [C|CS]) :- sameSize(MS, CS).
 */
-
-
